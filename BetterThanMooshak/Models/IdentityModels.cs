c60924cf7f71a58176088986155418dab949ea7d
Danielos15 <danielos15@ru.is>
2016-05-03 12:55:43 +0000

    DB connection string rename fix

--- BetterThanMooshak/Models/IdentityModels.cs
+++ BetterThanMooshak/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace BetterThanMooshak.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("MooshakDB", throwIfV1Schema: false)
         {
         }
 
