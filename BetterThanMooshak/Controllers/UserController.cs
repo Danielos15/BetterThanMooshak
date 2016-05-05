3be715fd989e6ef7fd02d784460bac6292bdc29e
Danielos15 <danielos15@ru.is>
2016-05-05 10:42:16 +0000

    fontawesome added, working on userControler

---
+++ BetterThanMooshak/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using BetterThanMooshak.Models.ViewModel;
+using BetterThanMooshak.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BetterThanMooshak.Controllers
+{
+    
+    public class UserController : Controller
+    {
+        private UserService service = new UserService();
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Overview()
+        {
+            UserViewModel viewModel = service.GetAllUsers();
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Import()
+        {
+            return View();
+        }
+
+        public ActionResult Changepassword()
+        {
+            return View();
+        }
+
+        [HttpPost] //TODO: changePassword viewModel to pass inn.
+        public ActionResult Changepassword(UserViewModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ BetterThanMooshak/Services/UserService.cs
@@ -0,0 +1,28 @@
+﻿using BetterThanMooshak.Models;
+using BetterThanMooshak.Models.Entities;
+using BetterThanMooshak.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BetterThanMooshak.Services
+{
+    public class UserService
+    {
+        private ApplicationDbContext db;
+
+        public UserService()
+        {
+            db = new ApplicationDbContext();
+        }
+
+        public UserViewModel GetAllUsers()
+        {
+            UserViewModel users = new UserViewModel();
+            users.users = (from user in db.Users
+                            select user).ToList();
+            return users;
+        }
+    }
+}
