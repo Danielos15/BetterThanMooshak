5a97608a2e68ea45f5624f60df40c350da3409d2
vidar10 <vidare90@gmail.com>
2016-05-10 15:06:40 +0000

    Edit problem redirect

--- BetterThanMooshak/Controllers/ProblemController.cs
+++ BetterThanMooshak/Controllers/ProblemController.cs
@@ -74,7 +74,7 @@ namespace BetterThanMooshak.Controllers
                 return View(problem);
             }
 
-            return RedirectToAction("index");
+            return RedirectToAction("details", "assignment", new { id = problem.assignmentId});
         }
 
         public ActionResult AddTestcase (int? id)
