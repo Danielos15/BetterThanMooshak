443406517895fe7e0d6fc751182d91e34922be7c
vidar10 <vidare90@gmail.com>
2016-05-13 17:04:41 +0000

    Finish Unit test

--- UnitTest/Services/HomeServiceTest.cs
+++ UnitTest/Services/HomeServiceTest.cs
@@ -10,13 +10,16 @@ namespace UnitTest.Services
     [TestClass]
     public class HomeServiceTest
     {
-        private HomeService service;
         private MockDataContext mockDb = new MockDataContext();
 
         [TestInitialize]
         public void Initialize()
         {
-
+            //We had troubles with this unit test and couldn't create a fake user with ApplicationUser.
+            //And since HomeService class only has one public function that needes an user id as a parameter
+            //we got stuck.
+            //If we could have create a fake user, then we could add fake courses, assignments, etc. and then
+            //test out all the private functions in HomeService
         }
     }
 }
